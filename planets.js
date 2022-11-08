2df0718316e75538af2024409be1d2cf0949b299
kitaowmik <80123072+kitaowmik@users.noreply.github.com>
2022-11-08 12:20:11 -0800

    fixed file locations

--- planets.js
+++ planets.js
@@ -1,4 +1,4 @@
-import * as THREE from '../build/three.module.js';
+import * as THREE from './build/three.module.js';
 import {OrbitControls} from './src/OrbitControls.js';
 import { GLTFLoader } from './src/GLTFLoader.js';
 
